9a9cc669c92c80a1f2fc7e86ecb2efa6bb50c1ff
Domagalski <adadom@st.amu.edu.pl>
2018-06-18 01:36:16 +0200

    added rs2 conf

--- RS2config.php
+++ RS2config.php
@@ -1,7 +1,27 @@
 <?php
-   define('DB_SERVER', 'localhost:3036');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'rootpassword');
-   define('DB_DATABASE', 'database');
+//use testdb
+// tworzymy tabele users jesli nie istnieje
+//CREATE TABLE IF NOT EXISTS `users` (`id` int(11) NOT NULL AUTO_INCREMENT,`username` varchar(32) NOT NULL,`password` varchar(32) NOT NULL,PRIMARY KEY (`id`),UNIQUE KEY `username` (`username`));
+//jakis tam user adam adam
+//INSERT INTO users SET users.username = 'adam', users.password = 'adam';
+// tworzymy usera tylko do selecta i insera
+//CREATE USER 'regLog'@'%' IDENTIFIED BY 'zaq12wsx';
+//GRANT INSERT ON DATABASE.* TO 'regLog'@'%';
+//GRANT SELECT ON DATABASE.* TO 'regLog'@'%';
+//mysql -u tester -p
+
+// jak sprawdzic port
+// SHOW GLOBAL VARIABLES LIKE 'PORT';
+
+   define('DB_SERVER', '192.168.100.10:3306');
+   define('DB_USERNAME', 'regLog');
+   define('DB_PASSWORD', 'zaq12wsx');
+   define('DB_DATABASE', 'testdb');
    $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+   // Check connection
+    if($db === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
 ?>
+
