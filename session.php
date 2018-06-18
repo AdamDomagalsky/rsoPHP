dc286ee60514d2ee6c7842ea833f32054fc33284
Domagalski <adadom@st.amu.edu.pl>
2018-06-19 01:54:37 +0200

    reorg

--- session.php
+++ session.php
@@ -3,27 +3,6 @@
 $host = gethostname();
 require_once "{$host}config.php";
 
-function check_session(){
-
-    // If session variable is not set it will redirect to login page
-    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
-        header("location: login.php");
-        exit;
-    } else {
-        restore_session();
-    }
-}
-
-function restore_session(){
-    $rC = new Redis();
-    $rC->connect( REDIS_SERVER, REDIS_PORT );
-	$rC->auth(REDIS_PASSWORD);
-	
-	$ret = Session::unserialize($rC->get($key));
-	// $ret=json_decode($rC->get($key),true);
-    $rC->close();
-	return $ret;
-}
 
 // kodzik znalazlem na http://php.net/manual/en/function.session-decode.php#108037
 class Session {
@@ -74,4 +53,29 @@ class Session {
         return $return_data;
     }
 }
+
+
+
+function check_session(){
+
+    // If session variable is not set it will redirect to login page
+    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] != true) {
+        header("location: login.php");
+        exit;
+    } else {
+        restore_session();
+    }
+}
+
+function restore_session(){
+    $rC = new Redis();
+    $rC->connect( REDIS_SERVER, REDIS_PORT );
+	$rC->auth(REDIS_PASSWORD);
+	
+	$ret = Session::unserialize($rC->get($key));
+	// $ret=json_decode($rC->get($key),true);
+    $rC->close();
+	return $ret;
+}
+
 ?>
