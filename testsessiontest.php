db07691a1d62e069c58413c12feb1dffe79e461a
Domagalski <adadom@st.amu.edu.pl>
2018-06-19 02:09:38 +0200

    1 arg to restor function

--- testsessiontest.php
+++
@@ -1,8 +0,0 @@
-<?php
-//simple counter to test sessions. should increment on each page reload.
-//
-session_start();
-$count = isset($_SESSION['count']) ? $_SESSION['count'] : 1;
-$host = gethostname();
-echo "$host $count";
-$_SESSION['count'] = ++$count;
